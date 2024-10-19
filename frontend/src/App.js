6b7a43d657751b6de4fe0805902e71880a482080
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-19 22:48:06 +0200

    adding navbar styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import Navbar from "./components/NavBar.js";
+import { CiMenuBurger } from "react-icons/ci";
+import MiniLogo from "./assets/mini-logo2.png";
+import { useState } from "react";
 
 function App() {
+  const [navExist, setNavExist] = useState(false);
   return (
     <div className="App">
-      <Navbar />
+      <div className="bg-[rgb(36,36,36)] h-screen w-full text-white">
+        {navExist && <Navbar setNavExist={setNavExist} />}
+        <heading className="flex flex-row items-center pt-[150px] ml-[100px] border-b-[1px] w-[900px] border-gray-500">
+          <CiMenuBurger
+            className="text-gray-300"
+            onClick={() => {
+              setNavExist(true);
+            }}
+          />
+          <img src={MiniLogo} className="w-[100px] h-auto pt-[3px]" />
+        </heading>
+      </div>
     </div>
   );
 }
