65808630a2540f16da4dd07737841eee0632ad39
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-19 23:36:19 +0200

    adding manage user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/NavBar.js";
 import { CiMenuBurger } from "react-icons/ci";
 import MiniLogo from "./assets/mini-logo2.png";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ManageUser from "./components/ManageUser.js";
 
 function App() {
   const [navExist, setNavExist] = useState(false);
@@ -10,7 +12,7 @@ function App() {
     <div className="App">
       <div className="bg-[rgb(36,36,36)] h-screen w-full text-white">
         {navExist && <Navbar setNavExist={setNavExist} />}
-        <heading className="flex flex-row items-center pt-[150px] ml-[100px] border-b-[1px] w-[900px] border-gray-500">
+        <heading className="flex flex-row items-center pt-[150px] ml-[100px] border-b-[1px] w-[1100px] border-gray-500">
           <CiMenuBurger
             className="text-gray-300"
             onClick={() => {
@@ -19,6 +21,13 @@ function App() {
           />
           <img src={MiniLogo} className="w-[100px] h-auto pt-[3px]" />
         </heading>
+        <section>
+          <Router>
+            <Routes>
+              <Route element={<ManageUser />} path="/" />
+            </Routes>
+          </Router>
+        </section>
       </div>
     </div>
   );
--- frontend/src/components/ManageUser.js
+++ frontend/src/components/ManageUser.js
@@ -1,7 +1,70 @@
 import React from "react";
-
+import { FaUsers } from "react-icons/fa";
 const ManageUser = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex flex-row space-x-[10px] items-center mt-[20px]">
+        <FaUsers /> <p className="font-bold">MANAGE USERS</p>
+      </div>
+      <form className="border-gray-500 border-[1px] mt-[20px] w-[700px] flex flex-col items-center rounded-[20px] h-[230px] mb-[50px] p-0">
+        <div className="h-[3px] w-[200px]  bg-white rounded-br-[5px] rounded-bl-[5px]"></div>
+        <div className="mt-[10px] grid grid-cols-2 gap-[10px]">
+          <div className="flex flex-col space-y-[7px] items-start">
+            <label
+              for="firstName"
+              className="text-[15px] font-bold text-gray-400"
+            >
+              First name :
+            </label>
+            <input
+              type="text"
+              placeholder="first name"
+              className="p-[4px] w-[300px] bg-[#242424] border-[1px] border-gray-500 placeholder:font-thin placeholder:text-[14px] px-[4px] rounded-[7px] focus:outline-none text-[15px]"
+            />
+          </div>
+          <div className="flex flex-col space-y-[7px] items-start">
+            <label
+              for="firstName"
+              className="text-[15px] font-bold text-gray-400"
+            >
+              Second name :
+            </label>
+            <input
+              type="text"
+              placeholder="first name"
+              className="p-[4px] w-[300px] bg-[#242424] border-[1px] border-gray-500 placeholder:font-thin placeholder:text-[14px] px-[4px] rounded-[7px] focus:outline-none text-[15px]"
+            />
+          </div>
+          <div className="flex flex-col space-y-[7px] items-start">
+            <label
+              for="firstName"
+              className="text-[15px] font-bold text-gray-400"
+            >
+              Phone :
+            </label>
+            <input
+              type="number"
+              placeholder="+ phone"
+              className="p-[4px] w-[300px] bg-[#242424] border-[1px] border-gray-500 placeholder:font-thin placeholder:text-[14px] px-[4px] rounded-[7px] focus:outline-none text-[15px]"
+            />
+          </div>
+          <div className="flex flex-col space-y-[7px] items-start">
+            <label for="Email" className="text-[15px] font-bold text-gray-400">
+              Email :
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="p-[4px] w-[300px] bg-[#242424] border-[1px] border-gray-500 placeholder:font-thin placeholder:text-[14px] px-[4px] rounded-[7px] focus:outline-none text-[15px]"
+            />
+          </div>
+        </div>
+        <button className="text-[#242424] bg-white mt-[20px] text-[15px] font-bold p-[5px] hover:bg-gray-300 rounded-[30px] w-[100px]">
+          Add user
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ManageUser;
