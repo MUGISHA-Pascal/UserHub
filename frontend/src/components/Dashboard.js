11e34754538862f00bffae1308cfc7c6ff2ad93c
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-21 06:39:24 +0200

    adding filter by search

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
-  return <div></div>;
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredusers, setFiltered] = useState([]);
+  let filteredUsers;
+  useEffect(() => {
+    fetch("https://userhub-xmb9.onrender.com/app/All-users")
+      .then((response) => response.json())
+      .then((usersdata) => {
+        setUsers(usersdata.users);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  useEffect(() => {
+    if (search === "") {
+      filteredUsers = users;
+      setFiltered(filteredUsers);
+    } else {
+      filteredUsers = users.filter((items) => {
+        return items.FirstName.toLowerCase().includes(search.toLowerCase());
+      });
+      setFiltered(filteredUsers);
+    }
+    console.log(filteredusers);
+  }, [users, search]);
+  return (
+    <div>
+      <div>
+        <label htmlFor="search">filter by First name :</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          value={search}
+          id="search"
+          className="bg-[#242424] border-[1px] focus:outline-none"
+        />
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>First-name</th>
+            <th>Second-name</th>
+            <th>Phone</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredusers.map((user) => {
+            <tr key={user._id}>
+              <td>{user.FirstName}</td>
+              <td>{user.SecondName}</td>
+              <td>{user.Phone}</td>
+              <td>{user.Email}</td>
+            </tr>;
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Dashboard;
