1a013efb454069128eab0cf2fc695a1caf0bb3a6
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-21 10:06:55 +0200

    frontend

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,8 +3,6 @@ import React, { useEffect, useState } from "react";
 const Dashboard = () => {
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
-  const [filteredusers, setFiltered] = useState([]);
-  let filteredUsers;
   useEffect(() => {
     fetch("https://userhub-xmb9.onrender.com/app/All-users")
       .then((response) => response.json())
@@ -15,22 +13,20 @@ const Dashboard = () => {
         console.log(error);
       });
   }, []);
-  useEffect(() => {
-    if (search === "") {
-      filteredUsers = users;
-      setFiltered(filteredUsers);
-    } else {
-      filteredUsers = users.filter((items) => {
-        return items.FirstName.toLowerCase().includes(search.toLowerCase());
-      });
-      setFiltered(filteredUsers);
-    }
-    console.log(filteredusers);
-  }, [users, search]);
+  const filteredusers =
+    search === ""
+      ? users
+      : users.filter((items) =>
+          items.FirstName.toLowerCase().includes(search.toLowerCase())
+        );
+  console.log(filteredusers);
+
   return (
-    <div>
-      <div>
-        <label htmlFor="search">filter by First name :</label>
+    <div className="flex flex-col items-center space-y-[20px]">
+      <div className="mt-[20px] flex flex-col items-start">
+        <label htmlFor="search" className="font-bold">
+          Filter by First name :
+        </label>
         <input
           type="text"
           onChange={(e) => {
@@ -38,27 +34,27 @@ const Dashboard = () => {
           }}
           value={search}
           id="search"
-          className="bg-[#242424] border-[1px] focus:outline-none"
+          className="bg-[#242424] border-[1px] rounded-[5px] focus:outline-none"
         />
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>First-name</th>
-            <th>Second-name</th>
-            <th>Phone</th>
-            <th>Email</th>
+      <table className="w-[800px] border-[1px] rounded-[10px]">
+        <thead className="border-[1px] rounded-[10px]">
+          <tr className="border-[1px]">
+            <th className="border-[1px]">First-name</th>
+            <th className="border-[1px]">Second-name</th>
+            <th className="border-[1px]">Phone</th>
+            <th className="border-[1px]">Email</th>
           </tr>
         </thead>
-        <tbody>
-          {filteredusers.map((user) => {
-            <tr key={user._id}>
-              <td>{user.FirstName}</td>
-              <td>{user.SecondName}</td>
-              <td>{user.Phone}</td>
-              <td>{user.Email}</td>
-            </tr>;
-          })}
+        <tbody className="border-[1px]">
+          {filteredusers.map((user) => (
+            <tr key={user._id} className="text-white border-[1px]">
+              <td className="border-[1px]">{user.FirstName}</td>
+              <td className="border-[1px]">{user.SecondName}</td>
+              <td className="border-[1px]">{user.Phone}</td>
+              <td className="border-[1px]">{user.Email}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
